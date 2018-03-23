f1a9b0dee4e5d500401e20de7871342ddf93bbf0
unknown <engmohammmad1984@yahoo.com>
2018-03-23 13:15:38 +0100

    Added Spring Boot files

---
+++ src/main/java/at/refugeescode/Mp4The/English/Gentelman/Mp4TheEnglishGentelmanApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.Mp4The.English.Gentelman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp4TheEnglishGentelmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp4TheEnglishGentelmanApplication.class, args);
+	}
+}
